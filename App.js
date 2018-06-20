6abf517d51607e8948cab5da16ace14151698381
Jonathan Tong <jt572@cornell.edu>
2018-06-20 00:20:03 -0400

    Followed iOS stopwatch tutorial on Medium and have a working stopwatch UI, except for laps

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { StyleSheet, TouchableHighlight, ListView } from 'react-native';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text, View, Left, Body, Right, Title } from 'native-base';
 import { createSwitchNavigator } from 'react-navigation';
+import TimeFormatter from 'minutes-seconds-milliseconds';
 
 export class FooterButton extends Component {
 
@@ -31,17 +33,246 @@ export class FooterTabNav extends Component {
 export class HeaderTitle extends Component {
   render() {
     return (
-      <Container>
-        <Header>
-          <Left/>
-          <Body>
-            <Title>{this.props.text}</Title>
-          </Body>
-          <Right />
-        </Header>
-      </Container>
+      // <Container>
+	    <Header>
+	      <Left/>
+	      <Body>
+	        <Title>{this.props.text}</Title>
+	      </Body>
+	      <Right />
+	    </Header>
+      // </Container>
     );
   }
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	header: {
+		borderBottomWidth: 0.5,
+		paddingTop: 20,
+		paddingBottom: 10,
+		backgroundColor: '#F9F9F9'
+	},
+	title: {
+		alignSelf: 'center',
+		fontWeight: '600',
+	},
+	timerWrapper: {
+		backgroundColor: '#FFFFFF',
+		justifyContent: 'center',
+		flex: 1
+	},
+	top: {
+		flex: 1,
+	},
+	bottom: {
+		flex: 2,
+		backgroundColor: '#F0EFF5'
+	},
+
+	mainTimer: {
+		fontSize: 80,
+		fontWeight: '100',
+		// borderWidth: 0.5,
+		alignSelf: 'flex-end',
+		fontFamily: 'Helvetica Neue'
+		// fontFamily: 'Menlo'
+	},
+	lapTimer: {
+		fontSize: 25,
+		fontWeight: '100',
+		// borderWidth: 0.5,
+		alignSelf: 'flex-end',
+		fontFamily: 'Helvetica Neue'
+		// fontFamily: 'Proxima Nova'
+	},
+	timerWrapperInner: {
+		// borderWidth: 0.5,
+		alignSelf: 'center',
+	},
+	buttonWrapper: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		paddingTop: 15,
+		paddingBottom: 30,
+	},
+	button: {
+		height: 80,
+		width: 80,
+		borderRadius: 40,
+		backgroundColor: '#FFFFFF',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+	lapRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		height: 40,
+		paddingTop: 10,
+		borderBottomWidth: 0.5,
+		borderBottomColor: '#ddd'
+	},
+	lapNumber: {
+		fontSize: 16,
+		color: '#777'
+	},
+	lapTime: {
+		color: '#000',
+		fontSize: 20,
+		fontWeight: '300'
+	},
+
+	startBtn: {
+		color: '#00cc00'
+	},
+	stopBtn: {
+		color: 'red'
+	},
+});
+
+let laps = [
+	{ name: 'Lap 1', value: '00.00.01' },
+	{ name: 'Lap 2', value: '00.00.02' },
+	{ name: 'Lap 3', value: '00.00.03' },
+	{ name: 'Lap 4', value: '00.00.04' },
+	{ name: 'Lap 5', value: '00.00.05' },
+];
+let ds = new ListView.DataSource({
+	rowHasChanged: (row1, row2) => row1 !== row2,
+});
+
+
+class Stopwatch extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			dataSource: ds.cloneWithRows(laps),
+			isRunning: false,
+			mainTimer: null,
+			lapTimer: null,
+			mainTimerStart: null,
+			lapTimerStart: null,
+		}
+	}
+
+	_renderLaps() {
+		return (
+			<View style={styles.lapsWrapper}>
+				<ListView
+					enableEmptySections={true}
+					dataSource={this.state.dataSource}
+					renderRow={ (rowData) => (
+						<View style={styles.lapRow}>
+							<Text style={styles.lapNumber}>{rowData.name}</Text>
+							<Text style={styles.lapNumber}>{rowData.value}</Text>
+						</View>
+					)}
+				/>
+			</View>
+		);
+	}
+
+	_renderTimers() {
+		return (
+			<View style={styles.timerWrapper}>
+				<View style={styles.timerWrapperInner}>
+					<Text style={styles.lapTimer}>{ TimeFormatter(this.state.lapTimer) }</Text>
+					<Text style={styles.mainTimer}>{ TimeFormatter(this.state.mainTimer) }</Text>
+				</View>
+			</View>
+		);
+	}
+
+	_renderButtons() {
+		return(
+			<View style={styles.buttonWrapper}>
+				<TouchableHighlight underlayColor='#777' onPress={this.handleLapReset.bind(this)} style={styles.button}>
+					<Text>{ (this.state.mainTimerStart && !this.state.isRunning) ? 'Reset' : 'Lap'}</Text>
+				</TouchableHighlight>
+				<TouchableHighlight underlayColor='#ddd' onPress={this.handleStartStop.bind(this)} style={styles.button}>
+					<Text style={[styles.startBtn, this.state.isRunning && styles.stopBtn]}>{this.state.isRunning ? 'Stop' : 'Start'}</Text>
+				</TouchableHighlight>
+
+			</View>
+		);
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.top}>
+					{this._renderTimers()}
+				</View>
+				<View style={styles.bottom}>
+					{this._renderButtons()}
+					{this._renderLaps()}
+				</View>
+			</View>
+		);
+	}
+
+	handleStartStop() {
+		let { isRunning, firstTime, mainTimer, lapTimer } = this.state;
+
+		// Case 1: stop button clicked
+		if(isRunning) {
+			clearInterval(this.interval);
+			this.setState({
+				isRunning: false
+			});
+
+			return;
+		}
+
+		// Case 2: start button clicked
+		this.setState({
+			mainTimerStart: new Date(),
+			lapTimerStart: new Date(),
+			isRunning: true
+		});
+
+		this.interval = setInterval(() => {
+			this.setState({
+				mainTimer: new Date() - this.state.mainTimerStart + mainTimer,
+				lapTimer: new Date() - this.state.lapTimerStart + lapTimer,
+			});
+		}, 30);
+	}
+
+	handleLapReset() {
+		let {isRunning, mainTimerStart} = this.state;
+
+		// Case 1: reset button clicked
+		if(mainTimerStart && !isRunning) {
+			laps: [],
+			this.setState({
+				mainTimerStart: null,
+				lapTimerStart: null,
+				mainTimer: 0,
+				lapTimer: 0,
+			});
+		}
+
+		// Case 2: lap button clicked
+		// if(mainTimerStart && isRunning) {
+		// 	laps: [],
+		// 	this.setState({
+		// 		mainTimerStart: null,
+		// 		lapTimerStart: null,
+		// 		mainTimer: 0,
+		// 		lapTimer: 0,
+		// 	});
+		// }
+
+	}
+
+
 }
 
 export class Timer extends Component {
@@ -55,11 +286,9 @@ export class Timer extends Component {
 			<Container>
 				<HeaderTitle text="Timer" />
                 {/* <Header><Title>Timer</Title></Header> */}
-				<Content>
-					<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-					  <Text>Hello, world!!</Text>
-					</View>
-				</Content>
+				{/* <Content style={{flex: 1}}> */}
+				<Stopwatch />
+				{/* </Content> */}
 				<Footer>
 					<FooterTabNav active="Timer" navigation={this.props.navigation}/>
 				</Footer>
@@ -78,7 +307,9 @@ export class Stats extends Component {
 		return (
 			<Container>
 				<HeaderTitle text="Statistics" />
-				<Content />
+				<Content>
+
+				</Content>
 				<Footer>
 					<FooterTabNav active="Stats" navigation={this.props.navigation}/>
 				</Footer>
